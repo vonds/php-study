7296d2d5a2ef55f302ab2d5453e87f6b34a519e5
Vonds <vonds13@gmail.com>
2018-12-15 19:27:28 -0500

    variables lesson

---
+++ variables.php
@@ -0,0 +1,51 @@
+
+
+<h1><?php 
+ 
+// Single line comment
+# Single line comment
+/*
+Multiline
+comment 
+*/
+
+// Varaibles 
+/*
+  -Prefix $
+  -Start with a letter or an underscore
+  -only letter, numbers, and underscores
+  -Case sensitive
+*/
+
+// Data Types
+/* 
+  String
+  Intergers
+  Floats
+  Booleans
+  Arrays
+  Objects
+  NULL
+  Resource
+*/
+
+$output = 'Hello Cow!';
+$num1 = 4;
+$num2 = 10;
+$sum = $num1 + $num2;
+
+$string1 = "Milk";
+$string2 = "boi";
+$greeting = "$string1 $string2";
+
+$flot1 = 4.4;
+$bool1 = true;
+$string3 = 'They\'re here';
+
+define('GREETING', 'I like dancing with my friends!!!!');
+
+echo GREETING;
+
+
+
+?></h1>
