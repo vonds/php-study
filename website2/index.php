f69f5053ee078b6b3300b7302408c1f61eef117e
Vonds <vonds13@gmail.com>
2018-12-16 08:20:53 -0500

    Superglobal lesson

---
+++ website2/index.php
@@ -0,0 +1,38 @@
+<?php include 'server-info.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <title>System Info</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Server and File Info</h1>
+    <?php if($server): ?>
+      <ul class="list-group">
+        <?php foreach($server as $key => $value): ?>
+          <li class="list-group-item">
+            <strong><?php echo $key; ?></strong>
+            <?php echo $value; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+
+    <h1>Client Info</h1>
+    <?php if($client): ?>
+      <ul class="list-group">
+        <?php foreach($client as $key => $value): ?>
+          <li class="list-group-item">
+            <strong><?php echo $key; ?></strong>
+            <?php echo $value; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+  </div>
+</body>
+</html>
